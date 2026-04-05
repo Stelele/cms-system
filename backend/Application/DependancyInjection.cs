4d1ecf71fd17892934cd7ce72dd80bbda4abb0c3
Stelele <giftmugweni@gmail.com>
2026-04-05 16:39:10 +0200

    fixing dependancies for docker image

--- backend/Application/DependancyInjection.cs
+++ backend/Application/DependancyInjection.cs
@@ -3,6 +3,7 @@ using Application.Posts;
 using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -16,8 +17,15 @@ public static class DependancyInjection
 
     public static WebApplicationBuilder AddApplication(this WebApplicationBuilder builder)
     {
+        var mediatRLicenseKey = builder.Configuration.GetValue<string>("MediatR:LicenseKey");
+        mediatRLicenseKey ??= Environment.GetEnvironmentVariable("MediatR__LicenseKey");
+
         builder.Services.AddMediatR(cfg =>
         {
+            if (!string.IsNullOrEmpty(mediatRLicenseKey))
+            {
+                cfg.LicenseKey = mediatRLicenseKey;
+            }
             cfg.RegisterServicesFromAssembly(typeof(CreatePostCommand).Assembly);
         });
         builder.Services.AddValidatorsFromAssembly(typeof(CreatePostCommand).Assembly);
