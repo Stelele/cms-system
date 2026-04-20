54d00c55f24f3276e7bc7c4e6e693b1741c8be0b
Gift Mugweni <giftmugweni@gmail.com>
2026-04-20 22:33:30 +0200

    feat: add BlogIcon migration

--- backend/Infrastructure/Infrastructure/Migrations/20260420203325_AddBlogIcon.Designer.cs
+++ backend/Infrastructure/Infrastructure/Migrations/20260420203325_AddBlogIcon.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Infrastructure.Migrations
 {
     [DbContext(typeof(CmsDbContext))]
-    partial class CmsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260420203325_AddBlogIcon")]
+    partial class AddBlogIcon
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.11");
---
+++ backend/Infrastructure/Infrastructure/Migrations/20260420203325_AddBlogIcon.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBlogIcon : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Icon",
+                table: "Blogs",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Icon",
+                table: "Blogs");
+        }
+    }
+}
