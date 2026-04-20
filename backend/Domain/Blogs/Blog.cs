7f7287691b3122aa838d89eaa358b470f34b77b9
Gift Mugweni <giftmugweni@gmail.com>
2026-04-20 22:31:50 +0200

    feat: add Icon property to Blog entity

--- backend/Domain/Blogs/Blog.cs
+++ backend/Domain/Blogs/Blog.cs
@@ -8,20 +8,23 @@ public class Blog : Base
     public string Name { get; set; } = string.Empty;
     public string Slug { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public string Icon { get; set; } = "i-heroicons-book-open";
 
     public List<Post> Posts { get; set; } = [];
 
     public static Blog Create(
         string name,
         string slug,
-        string description)
+        string description,
+        string icon)
     {
         return new Blog
         {
             Id = Guid.NewGuid(),
             Name = name,
             Slug = slug,
-            Description = description
+            Description = description,
+            Icon = icon
         };
     }
 }
